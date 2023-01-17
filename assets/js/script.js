aab7cf5e676c8dde7abb9cf48168fc7368205d74
fazal <fazal@ksystem.com.np>
2023-01-17 21:57:06 +0545

    Added sound camera and antialiasing to renderer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,11 @@ import { OrbitControls } from "./OrbitControls.js";
 import { MTLLoader } from "./MTLLoader.js";
 import { OBJLoader } from "./OBJLoader.js";
 
+const loadingScreenEle = document.getElementById("LoadingScreen");
+loadingScreenEle.style.display = "flex";
+const loadingScreenProgressEle = document.getElementById("LoadingScreenProgress");
+loadingScreenProgressEle.value = 0;
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
 
@@ -10,31 +15,37 @@ const light = new THREE.AmbientLight(0xffffff);
 light.intensity = 3;
 scene.add(light);
 
-
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 0, 20);
 
-const renderer = new THREE.WebGLRenderer();
+const listener = new THREE.AudioListener();
+camera.add(listener);
+const sound = new THREE.Audio(listener);
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load("./assets/models/dhokra/a-small-miracle-132333.mp3", function (buffer) {
+    sound.setBuffer(buffer);
+    sound.setLoop(true);
+    sound.setVolume(0.5);
+    sound.play();
+});
+
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
+controls.autoRotate = true;
 
 const mtlLoader = new MTLLoader();
 mtlLoader.load(
-    './assets/models/dhokra/Dhokra.mtl',
+    "./assets/models/dhokra/Dhokra.mtl",
     (materials) => {
         materials.preload();
         const objLoader = new OBJLoader();
         objLoader.setMaterials(materials);
         objLoader.load(
-            './assets/models/dhokra/Dhokra.obj',
+            "./assets/models/dhokra/Dhokra.obj",
             function (object) {
                 object.traverse(function (child) {
                     if (child instanceof THREE.Mesh) {
@@ -46,10 +57,14 @@ mtlLoader.load(
             function (xhr) {
                 if (xhr.loaded === xhr.total) {
                     console.log("Object loaded");
+                    loadingScreenEle.style.display = "none";
+                    loadingScreenProgressEle.value = 0;
+                } else {
+                    loadingScreenProgressEle.value = (xhr.loaded / xhr.total) * 100;
                 }
             },
             function (error) {
-                console.log('An error happened');
+                console.log("An error happened");
             }
         );
     },
@@ -59,11 +74,11 @@ mtlLoader.load(
         }
     },
     (error) => {
-        console.log('An error happened');
+        console.log("An error happened");
     }
 );
 
-window.addEventListener('resize', onWindowResize, false)
+window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
